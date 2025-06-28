9bacad25aa3dd7e871175579de28977eb1f12a36
Mohammed Musharaf <mohamedmusharaf687@gmail.com>
2025-06-28 19:35:01 +0530

    basic hello world implemented. credits: springboot

--- src/main/java/com/example/parcel/ParcelApplication.java
+++ src/main/java/com/example/parcel/ParcelApplication.java
@@ -2,12 +2,21 @@ package com.example.parcel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@RestController
 @SpringBootApplication
 public class ParcelApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ParcelApplication.class, args);
 	}
+
+  @GetMapping("/hello")
+  public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+    return String.format("Hello %s!", name);
+  }
 
 }
